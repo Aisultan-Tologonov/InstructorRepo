069f0cb7c1f97b6aca850bc63c64fef4a4d528c1
Aisultan-Tologonov <230402384@aston.ac.uk>
2025-01-23 10:40:12 +0000

    Update Lab01_Java.java
    changes made by student

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  //Edit made by Student-230402384
+  //Edit made by AisultanTologonov-230402384
 
   //Last line print('TA TA')
 }
